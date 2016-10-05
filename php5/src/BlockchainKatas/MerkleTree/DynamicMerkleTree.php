20aaf25d9c7b4100766c5f48bbfd02759820412f
Carlos López <cladev@gmail.com>
2016-10-05 22:27:06 +0000

    +josecelano: better memory implementation for dynamic Merkle root

--- php5/src/BlockchainKatas/MerkleTree/DynamicMerkleTree.php
+++ php5/src/BlockchainKatas/MerkleTree/DynamicMerkleTree.php
@@ -4,21 +4,39 @@ namespace BlockchainKatas\MerkleTree;
 
 class DynamicMerkleTree
 {
-    private $merkleTree;
+    private $hash;
     private $chunks;
 
     public function __construct(callable $hash)
     {
-        $this->merkleTree = new RecursiveMerkleTree($hash);
+        $this->hash = $hash;
     }
 
     public function addChunk($chunk)
     {
-        $this->chunks[] = $chunk;
+        $this->hashes[] = $this->h($chunk);
     }
 
     public function calculateMerkleRoot()
     {
-        return $this->merkleTree->calculateMerkleRoot($this->chunks);
+        $hashes = $this->hashes;
+        if (count($hashes) % 2) {
+            $hashes[] = $hashes[count($hashes) - 1];
+        }
+
+        while (count($hashes)>=2) {
+            $aux = [];
+            for($i=0;$i<count($hashes);$i+=2) {
+                $aux[] = $this->h($hashes[$i] . $hashes[$i+1]);
+            }
+            $hashes = $aux;
+        }
+        return $hashes[0];
+
+    }
+
+    private function h($chunk)
+    {
+        return call_user_func_array($this->hash, [$chunk]);
     }
 }
